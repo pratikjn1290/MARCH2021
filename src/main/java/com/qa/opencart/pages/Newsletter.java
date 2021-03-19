fe4dfeabc91d38c658f1618277d74fc57e937d21
Pratik Jain <pratik.jain2@outlook.com>
2021-03-19 22:50:15 +0530

    Create Newsletter.java

---
+++ src/main/java/com/qa/opencart/pages/Newsletter.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Newsletter {
+	private WebDriver driver;
+
+	public Newsletter(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
