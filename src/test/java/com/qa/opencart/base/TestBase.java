ce56fc74bf13e5b9eb44be97b5faa0b898cb9e68
pratikjn1290 <pratik.jain2@outlook.com>
2021-03-23 11:10:31 +0530

    changes related to remote and local run

--- src/test/java/com/qa/opencart/base/TestBase.java
+++ src/test/java/com/qa/opencart/base/TestBase.java
@@ -7,9 +7,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import com.beust.jcommander.Parameter;
 import com.qa.opencart.driverfactory.DriverFactory;
-import com.qa.opencart.driverfactory.OptionManager;
 import com.qa.opencart.pages.AccountPage;
 import com.qa.opencart.pages.LoginPage;
 import com.qa.opencart.pages.ProductInfoPage;
@@ -27,20 +25,16 @@ public class TestBase extends DriverFactory {
 
 	@BeforeTest
 	@Parameters({"browser","version"})
-	public void setupBrowser(String bName, String bVersion) {
+	public void setupBrowser(String browserName, String browserVersion) {
 		df = new DriverFactory();
 		prop = df.initProperties();
-		String browserName = prop.getProperty("browser").trim();	
+		String browser = prop.getProperty("browser").trim();	
 		if(browserName!= null)
 		{
-			browserName = bName;
+			browser = browserName;
 		}
-		driver = df.initBrowser(browserName, bVersion);
+		driver = df.initBrowser(browser, browserVersion);
 		loginPage = new LoginPage(driver);
-		accountPage = new AccountPage(driver);
-		productSearchPage = new ProductSearchPage(driver);
-		productInfoPage = new ProductInfoPage(driver);
-
 	}
 
 	@AfterTest
